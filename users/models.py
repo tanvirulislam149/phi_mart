4b1bb3fda85e6eeaf8720eed811a1db421987cdf
tanvir <tanvirulislam149@gmail.com>
2025-02-20 19:49:01 +0600

    custom user

--- users/models.py
+++ users/models.py
@@ -1,20 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from users.managers import CustomUserManager
-
-
 # Create your models here.
+
 class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True)
     address = models.TextField(blank=True, null=True)
-    phone = models.CharField(max_length=15, blank=True, null=True)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
 
-    USERNAME_FIELD = "email"   # For using email instead of username
-    REQUIRED_FIELDS = []       # no field required like username
+    USERNAME_FIELD = 'email'  # Use email instead of username
+    REQUIRED_FIELDS = []
 
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.email 
-
+        return self.email
